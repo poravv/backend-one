8124b19a37907577de36459dcc0d43e6e4c2e9ad
Andres Vera <andyvercha@gmail.com>
2023-11-30 22:03:47 -0300

    upd one service

--- src/middleware/validacion_about.js
+++ src/middleware/validacion_about.js
@@ -5,7 +5,6 @@ const validateCreate = [
     check('title').exists().not().isEmpty().withMessage('Favor cargar un título'), 
     check('subtitle').exists().not().isEmpty().withMessage('Favor cargar un subtítulo'), 
     check('description').exists().not().isEmpty().withMessage('Favor cargar una descripción'), 
-    check('html_image').exists().not().isEmpty().withMessage('Favor cargar url de imagen'),
     check('about_background').exists().not().isEmpty().withMessage('Favor cargar url de imagen'),
     //check('email').exists().not().isEmpty().isEmail().withMessage('Favor cargar un email valido'),
     (req,res,next) => {
